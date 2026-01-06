b0b64cee8a7b7fd9d3c79eb80ace662e0ccc7d4f
Fabio Scagliola <scagliola@gmail.com>
2026-01-06 20:08:18 +0100

    Empty test project

---
+++ Core.Presentation.Test/WhateverTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace com.fabioscagliola.Core.Presentation.Test;
+
+public class WhateverTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestWhatever()
+    {
+        Assert.Pass();
+    }
+}
